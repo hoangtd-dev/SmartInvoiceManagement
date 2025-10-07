0b4c2f6b7efea7f6c52ba8023206ba38bb3d15d3
Edward <hoangtd.dev@gmail.com>
2025-10-07 23:16:32 +1100

    Set up sidebar with rout

---
+++ SIM.Presentation/Pages/Account/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SIM.Presentation.Pages.Account
+{
+    public class AccountModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
