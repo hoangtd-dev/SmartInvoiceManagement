f597ce26e412915c417f0a706649b86f798fd47a
Hai Thanh Le <haithanhle1510@gmail.com>
2025-10-09 11:21:44 +1100

    fix: ui style

--- SIM.Presentation/Pages/Welcome.cshtml.cs
+++ SIM.Presentation/Pages/Welcome.cshtml.cs
@@ -5,6 +5,9 @@ namespace SIM.Presentation.Pages
     public class WelcomeModel : PageModel
     {
         public string TitleText { get; private set; } = "Welcome";
+        // Page links used by the Welcome.cshtml view
+        public string LoginPage { get; private set; } = "/Account/Login";
+        public string RegisterPage { get; private set; } = "/Account/Register";
         public void OnGet()
         {
             ViewData["Title"] = TitleText;
